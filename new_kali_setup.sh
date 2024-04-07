7eda2e7c0008073501d16ad7938c3a62239ad65e
nniikon <kolya.kasparov@gmail.com>
2024-04-07 15:26:11 +0300

    add new commands

--- new_kali_setup.sh
+++ new_kali_setup.sh
@@ -3,5 +3,15 @@
 sudo apt install neovim
 sudo apt install xournalpp
 sudo apt install keepass2 
+sudo apt install telegram-desktop
+sudo apt install cmake
+# Neovim won't build without it
+sudo apt install gettext
+# sudo apt install texlive-full 
 
+# Build neovim
+git clone https://github.com/neovim/neovim
+cd neovim/
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
 
